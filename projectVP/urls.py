c69ceb28ee9e80d174ce941a5f0c36bb7e7acdb2
LuisP30 <pimenta.lh2@gmail.com>
2023-05-18 23:38:16 -0300

    commit test

--- projectVP/urls.py
+++ projectVP/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.http import HttpResponse 
 def my_view(request):
-    ...
+    return HttpResponse('Aprendendo HttpResponse')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
