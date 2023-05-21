a0ab17f786edeb44f282c7cbbe2bc69860d00c13
LuisP30 <pimenta.lh2@gmail.com>
2023-05-21 18:48:33 -0300

    change urls.py

--- projectVP/urls.py
+++ projectVP/urls.py
@@ -18,10 +18,19 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse 
 
-def my_view(request):
-    return HttpResponse('Aprendendo HttpResponse')
+def home(request):
+    return HttpResponse('Home')
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+def contato(request):
+    return HttpResponse('Contato')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view)
+    path('', home), # Home
+    path('sobre/', sobre), # /sobre/
+    path('contato/', contato), # /contato/
+
 ]
